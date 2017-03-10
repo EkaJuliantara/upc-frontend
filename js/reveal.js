17f359f87f8e085e74b78497f1509f0a0d6c10eb
rio gunawan <rio09tkj@gmail.com>
2017-03-10 10:56:31 +0700

    ini mastah

--- js/reveal.js
+++ js/reveal.js
@@ -1,12 +1,12 @@
-sr.reveal( "#bab1 .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#bab2 .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#bab3 .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#penutup .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#opening ", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#juduls .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#judulj .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#syarat .columns", { reset: true, viewOffset: { top: 64 } } );
-sr.reveal( "#cd-timeline .cd-timeline-block", { reset: true, viewOffset: {top: 64} } );
+sr.reveal( "#bab1 .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#bab2 .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#bab3 .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#penutup .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#opening ", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#juduls .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#judulj .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#syarat .columns", { reset: true, viewOffset: { top: 30 } } );
+sr.reveal( "#cd-timeline .cd-timeline-block", { reset: true, viewOffset: {top: 30} } );
   function addCommasToNum( num ){
     return num.toString().replace( /\B(?=(\d{3})+(?!\d))/g, "," );
   }
