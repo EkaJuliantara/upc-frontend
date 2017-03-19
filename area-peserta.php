a1d28206cb809da1f770e40ac528e396177dedac
EkaJuliantara <juliantaraeka@gmail.com>
2017-03-19 19:27:32 +0700

    tambah pengumuman

--- area-peserta.php
+++ area-peserta.php
@@ -149,6 +149,8 @@
                 <h5>Pengumuman</h5>
               </div>
               <div class="box-body">
+                <p>Sebelum mengirimkan proposal, harus melunasi biaya pendaftaran terlebih dahulu yaitu Rp 100.000,-</p>
+                <p>Untuk pembayaran dapat dilakukan langsung ke stand IFest di Lobby Fakultas Teknologi Industri, Kampus 3, Gedung Bonaventura, Jalan Babarsari 43 Yogyakarta atau melalui transfer ke <strong> Bank BCA dengan nomor rekening 0373749971 atas nama Grelly Lucia Yovellia Londo.</strong></p>
                 <p>Kecepatan koneksi internet mempengaruhi cepatnya data ditampilkan. Apabila data belum tertampil, silakan muat ulang halaman.</p>
                 <p>Kami menyarankan menggunakan browser Mozilla Firefox</p>
               </div>
